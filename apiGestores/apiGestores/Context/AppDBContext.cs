fdac1142e26b0ec6a95279a17fdaded06a8b8fa7
Joel Butti <joelbutti95@gmail.com>
2020-10-15 21:19:39 -0300

    init commit

---
+++ apiGestores/apiGestores/Context/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using apiGestores.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiGestores.Context
+{
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Gestores_BD> gestores_bd { get; set; } 
+    }
+}
---
+++ apiGestores/apiGestores/Models/Gestores_BD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiGestores.Models
+{
+    public class Gestores_BD
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int lanzamiento { get; set; }
+        public string desarrollador { get; set; }
+    }
+}
